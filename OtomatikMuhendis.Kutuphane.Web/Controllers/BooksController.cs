76679fa425f7515d96ec5b863dd5048b036b0fa9
olcay <olcaybayram@gmail.com>
2017-11-07 23:12:51 +0100

    Add a book view created

---
+++ OtomatikMuhendis.Kutuphane.Web/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OtomatikMuhendis.Kutuphane.Web.Data;
+using OtomatikMuhendis.Kutuphane.Web.ViewModels;
+using System.Linq;
+
+namespace OtomatikMuhendis.Kutuphane.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            var viewModel = new BookFormViewModel
+            {
+                Shelves = _context.Shelves.Select(p => new SelectListItem
+                {
+                    Text = p.Title,
+                    Value = p.Id.ToString()
+                }).ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ OtomatikMuhendis.Kutuphane.Web/ViewModels/BookFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OtomatikMuhendis.Kutuphane.Web.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OtomatikMuhendis.Kutuphane.Web.ViewModels
+{
+    public class BookFormViewModel
+    {
+        public string Title { get; set; }
+
+        public int ShelfId { get; set; }
+
+        public IEnumerable<SelectListItem> Shelves { get; set; }
+    }
+}
